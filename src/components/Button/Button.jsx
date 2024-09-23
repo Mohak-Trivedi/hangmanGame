4132e89a44906486353e7cb8e777b71b8688d44e
Mohak-Trivedi <trivedimohak@gmail.com>
2024-09-23 22:45:07 +0530

    separation of concern - ui and logic, start game page

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,5 @@
+import getButtonStyling from "./getButtonStyling";
+
 function Button({ text, onClickHandler, styleType = "primary" }) {
   return (
     <button
@@ -7,20 +9,6 @@ function Button({ text, onClickHandler, styleType = "primary" }) {
       {text}
     </button>
   );
-}
-
-function getButtonStyling(styleType) {
-  if (styleType === "primary") {
-    return "bg-blue-500";
-  } else if (styleType === "secondary") {
-    return "bg-gray-500";
-  } else if (styleType === "error") {
-    return "bg-red-500";
-  } else if (styleType === "success") {
-    return "bg-green-500";
-  } else if (styleType === "warning") {
-    return "bg-yellow-500";
-  }
 }
 
 export default Button;
