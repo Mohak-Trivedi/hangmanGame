ec486274438001d7a5f064903c1c6bf9f25762ad
Mohak-Trivedi <trivedimohak@gmail.com>
2024-09-25 23:34:46 +0530

    Link routing b/w start and play pages

--- src/pages/StartGame.jsx
+++ src/pages/StartGame.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function StartGame() {
   return (
     <>
       <h1>Start Game</h1>
-
+      <Link to="/play" className="text-blue-400">
+        Play Game
+      </Link>
       {/* Your comment goes here. */}
     </>
   );
