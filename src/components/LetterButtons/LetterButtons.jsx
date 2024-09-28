a9a486de25ea7099e5c5c4c7952ec15d3fc92b38
Mohak-Trivedi <trivedimohak@gmail.com>
2024-09-28 12:07:35 +0530

    guessing letters logic

--- src/components/LetterButtons/LetterButtons.jsx
+++ src/components/LetterButtons/LetterButtons.jsx
@@ -6,7 +6,7 @@ and disables the guessed letter and also styles it as per correct / incorrect
 guess.
 */
 function LetterButtons({ text, guessedLetters, onLetterClick }) {
-  const originalLettersSet = new Set(text.toUpperCase().split());
+  const originalLettersSet = new Set(text.toUpperCase().split(""));
   const guessedLettersSet = new Set(guessedLetters);
 
   function buttonStyle(alphabet) {
@@ -19,11 +19,17 @@ function LetterButtons({ text, guessedLetters, onLetterClick }) {
     }
   }
 
+  function handleLetterClick(event) {
+    const alphaOnButton = event.target.value;
+    onLetterClick?.(alphaOnButton);
+  }
+
   const buttons = ALPHABETS.map((alphabet) => {
     return (
       <button
         key={`btn-${alphabet}`}
-        onClick={onLetterClick}
+        value={alphabet}
+        onClick={handleLetterClick}
         disabled={guessedLettersSet.has(alphabet)}
         className={`h-12 w-12 m-1 text-white rounded-md ${buttonStyle(
           alphabet
--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,21 +1,28 @@
 import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import MaskedText from "../components/MaskedText/MaskedText";
 import LetterButtons from "../components/LetterButtons/LetterButtons";
 
 function PlayGame() {
   const { state } = useLocation();
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  function handleLetterClick(letter) {
+    setGuessedLetters([...guessedLetters, letter]);
+  }
+
   return (
     <>
-      <h1>Play Game {state.wordSelected}</h1>
+      <h1>Play Game</h1>
 
-      <MaskedText text={state.wordSelected} guessedLetters={[]} />
+      <MaskedText text={state.wordSelected} guessedLetters={guessedLetters} />
 
       <div>
         <LetterButtons
           text={state.wordSelected}
-          guessedLetters={[]}
-          onLetterClick={() => {}}
+          guessedLetters={guessedLetters}
+          onLetterClick={handleLetterClick}
         />
       </div>
 
