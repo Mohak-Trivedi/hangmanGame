384048fde1aa62485af1b7f3cb916f90fce2688e
Mohak-Trivedi <trivedimohak@gmail.com>
2024-09-29 16:52:02 +0530

    added feature of selecting random word to guessed for single player

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Button from "../components/Button/Button";
 
 function Home() {
+  const [word, setWord] = useState("");
+
+  async function fetchWords() {
+    const response = await fetch("http://localhost:3000/words");
+    const data = await response.json();
+    console.log(data);
+
+    const randomIndex = Math.floor(Math.random() * data.length);
+    console.log(data[randomIndex]);
+
+    setWord(data[randomIndex].wordValue);
+  }
+
+  useEffect(() => {
+    fetchWords();
+  }, []);
+
   return (
     <>
-      <Link to="/play" state={{ wordSelected: "MANGO" }}>
+      <Link to="/play" state={{ wordSelected: word }}>
         <Button text="Single Player Button" />
       </Link>
       <Link to="/start">
